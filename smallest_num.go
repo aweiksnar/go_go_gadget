a883d2350f7fe051db123a2aac4cd000cf22f78e
Alex Weiksnar <aweiksnar@Alexs-MacBook-Pro.local>
2013-10-09 11:50:51 -0500

    switch smallest_num func order

--- smallest_num.go
+++ smallest_num.go
@@ -12,16 +12,6 @@ package main
 
 import "fmt"
 
-func main() {
-	x := []int{
-		48, 96, 86, 68,
-		57, 82, 63, 70,
-		37, 34, 83, 27,
-		19, 97, 9, 17,
-	}
-	fmt.Println(smallestNum(x)) // 9
-}
-
 func smallestNum(nums []int) int {
 	smallNum := nums[0]
 	for _, value := range nums {
@@ -30,4 +20,14 @@ func smallestNum(nums []int) int {
 		}
 	}
 	return smallNum
+}
+
+func main() {
+	x := []int{
+		48, 96, 86, 68,
+		57, 82, 63, 70,
+		37, 34, 83, 27,
+		19, 97, 9, 17,
+	}
+	fmt.Println(smallestNum(x)) // 9
 }
